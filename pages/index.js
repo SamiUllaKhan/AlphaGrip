3343dc1f3ed36ca9eb0ac122bad6e92e33f2403a
Sami <sami.khan@plexussquare.com>
2021-09-28 18:34:55 +0530

    - Facebook chat added - Favicon updated

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
+import Script from 'next/script'
 import Head from 'next/head'
-import Footer from '../components/Footer'
-import Menu from '../components/GroupMenu'
 import Intro from '../components/Intro'
 import Main from '../components/Main'
 
@@ -16,7 +15,30 @@ export default function Home() {
       <Main />
 
       <Intro />
+      <div id="fb-root"></div>
+    <div id="fb-customer-chat" className="fb-customerchat"></div>
 
+    <Script strategy="lazyOnload"
+     dangerouslySetInnerHTML={{
+      __html: `
+      var chatbox = document.getElementById('fb-customer-chat');
+      chatbox.setAttribute("page_id", "106726951775061");
+      chatbox.setAttribute("attribution", "biz_inbox");
+
+      window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v12.0'
+        })
+      };
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));`}}
+    />
     </div>
   )
 }
